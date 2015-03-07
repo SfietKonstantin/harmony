47ec2897caee7772a9b29550a79d46883b1f5618
Lucien XU <sfietkonstantin@free.fr>
2015-03-07 10:21:01 +0100

    Fix for CI

--- tools/ci/ci-build.sh
+++ tools/ci/ci-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 mkdir -p build && cd build
-qmake-qt5 -r CONFIG+=desktop CONFIG+=debug CONFIG+=harmonydebug ..
+qmake-qt5 -r CONFIG+=desktop CONFIG+=debug CONFIG+=testing ..
 make -j11
